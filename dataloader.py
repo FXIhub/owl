f2b41db7a7c0f27ec3b8aafe84dcf7024439a2a4
Filipe Maia <filipe.c.maia@gmail.com>
2014-07-02 13:50:08 +0200

    Nicer status Messages

--- dataloader.py
+++ dataloader.py
@@ -313,6 +313,7 @@ class DataItem:
             self.tagMembers[tag,img] = 1
         else:
             self.tagMembers[tag,img] = 0
+        self.fileLoader.parent.statusBar.showMessage('Tag '+self.tags[tag][0]+' set to '+str(bool(value)))
         self.updateTagSum()
     def updateTagSum(self):
         for i in range(0,len(self.tags)):
