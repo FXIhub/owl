f2483add6b2ff1fb8d0fd7d561f9715dc65b2f5e
Max Hantke <maxhantke@gmail.com>
2014-07-15 18:00:17 +0200

    replaced my gaussian filter by the standard one from scipy.ndimage

--- patterson.py
+++ patterson.py
@@ -1,6 +1,7 @@
 import numpy
 import logging
 import scipy.signal
+import scipy.ndimage
 
 class PattersonCreator:
     def __init__(self,dataItemImage,dataItemMask):
@@ -14,30 +15,15 @@ class PattersonCreator:
         P = numpy.fft.fftshift(numpy.fft.fft2(numpy.fft.fftshift(K*I)))
         return P
 
+def patterson(img,mask,**kwargs):
+    smooth = kwargs.get("smooth",5.)
+    K = kernel(mask,smooth)
+    P = numpy.fft.fftshift(numpy.fft.fft2(numpy.fft.fftshift(K*img)))
+    return P
 
 def kernel(mask,smooth):
-    K = gaussian_smooth(mask,smooth)
+    K = scipy.ndimage.gaussian_filter(mask,smooth)
     K -= 0.5*K.max()
     K[K<0] = 0.
     K /= K.max()
     return K
-
-def gaussian_smooth(I,sm,precision=1.):
-    N = 2*int(numpy.round(precision*sm))+1
-    if len(I.shape) == 2:
-        X,Y = numpy.meshgrid(numpy.arange(0,N,1),numpy.arange(0,N,1))
-        X = X-N/2
-        Y = Y-N/2
-        R = numpy.sqrt(X**2 + Y**2)
-        kernel = numpy.exp(R**2/(2.0*sm**2))
-        kernel[abs(R)>N/2] = 0.
-        kernel /= kernel.sum()
-        Ism = scipy.signal.convolve2d(I,kernel,mode='same',boundary='fill')
-        return Ism
-    elif len(I.shape) == 1:
-        X = numpy.arange(0,N,1)
-        X = X-(N-1)/2.
-        kernel = numpy.exp(X**2/(2.0*sm**2))
-        kernel /= kernel.sum()
-        Ism = numpy.convolve(I,kernel,mode='same')
-        return Ism
