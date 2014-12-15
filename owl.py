b8b44cfafb5411925afde45f05521fcadb7a126c
Max Hantke <maxhantke@gmail.com>
2014-12-15 13:43:04 +0100

    added binary color scale to traditional color scales, requested by Anton

--- owl.py
+++ owl.py
@@ -336,7 +336,7 @@ class Owl(QtGui.QMainWindow):
         self.colormapMenu = QtGui.QMenu("Colormap", self)
         self.colormapActionGroup = QtGui.QActionGroup(self)
 
-        traditionalColormaps = ['jet', 'hot', 'gray', 'coolwarm', 'gnuplot', 'gist_earth']
+        traditionalColormaps = ['jet', 'hot', 'binary', 'gray', 'coolwarm', 'gnuplot', 'gist_earth']
         self.colormapActions = {}
         for colormap in traditionalColormaps:
             a = self.colormapMenu.addAction(colormapIcons.pop(colormap), colormap)
